d9df1999d47c4ae0096fd0a57430898786984633
安藤弘貴 <OIBC34242@ojs.ac.jp>
2023-04-24 09:54:24 +0900

    マウスクリックの位置からボール表示

--- Sample/BallApp/Program.cs
+++ Sample/BallApp/Program.cs
@@ -13,6 +13,10 @@ namespace BallApp {
         private SoccerBall soccerBall;
         private PictureBox pb;       //画像を表示するコントロール
 
+        private List<SoccerBall> balls = new List<SoccerBall>();      //ボールインスタンス格納用　同じことを書いてる
+        ///private var balls = new List<>();        //型推論型　ここでは無理　メソッド内でやって
+        private List<PictureBox> pbs = new List<PictureBox>();  //表示用
+
         static void Main(string[] args) {
 
             Application.Run(new Program());
@@ -29,33 +33,50 @@ namespace BallApp {
             this.Size = new Size(800, 600);     //上2つをいっきにやってくれる
             this.BackColor = Color.Green;       //背景色を緑にする
             this.Text = "BallGame";     //左上にン名前を表示
+            this.MouseClick += Program_Mouseclick;
 
             //form.Show();      //フォームを表示　Run使えば勝手にこれやってくれる
             //this.ShowDialog();      //フォームのダイアログを表示　Run使えば勝手にこれやってくれる
 
-            //ボールインスタンス
-            soccerBall = new SoccerBall();
-            pb = new PictureBox();       //画像を表示するコントロール
-            pb.Image = soccerBall.Image;        //イメージの引っ張り出し
-            pb.Location = new Point((int)soccerBall.PosX, (int)soccerBall.PosY);        //画像の位置　(int)はしゃあない
-            pb.Size = new Size(50, 50);     //画像の表示サイズ
-            pb.SizeMode = PictureBoxSizeMode.StretchImage;      //画像の表示モード
-
-            pb.Parent = this;       //no form  form = this ！
-
             moveTimer = new Timer();
             moveTimer.Interval = 1;     //タイマーのインターバル（ms）
             moveTimer.Start();      //タイマースタート
-            moveTimer.Tick += MoveTimer_Tick;
+            moveTimer.Tick += MoveTimer_Tick;       //デリゲート登録
 
             
 
         }
 
+        //マウスクリック時のイベントハンドラ
+        private void Program_Mouseclick( object sender , MouseEventArgs e ) {
+
+            //ボールインスタンス生成
+            soccerBall = new SoccerBall( e.X - 25 , e.Y - 25 );       //座標をもらう
+            pb = new PictureBox();       //画像を表示するコントロール
+            pb.Image = soccerBall.Image;        //イメージの引っ張り出し
+            pb.Location = new Point((int)soccerBall.PosX, (int)soccerBall.PosY);        //画像の位置　(int)はしゃあない
+            pb.Size = new Size(50, 50);     //画像の表示サイズ
+            pb.SizeMode = PictureBoxSizeMode.StretchImage;      //画像の表示モード
+            pb.Parent = this;       //no form  form = this ！
+
+            balls.Add(soccerBall);
+            pbs.Add(pb);
+
+            moveTimer.Start();      //タイマースタート
+
+        }
+
+        //タイマータイムアウト時のイベントハンドラ
         private void MoveTimer_Tick(Object sender, EventArgs e ) {
 
-            soccerBall.Move();     //移動
-            pb.Location = new Point((int)soccerBall.PosX, (int)soccerBall.PosY);        //画像の位置
+            for (int i = 0; i < balls.Count; i++)
+            {
+
+                balls[i].Move();     //移動
+                pbs[i].Location = new Point((int)balls[i].PosX, (int)balls[i].PosY);        //画像の位置
+
+            }
+
 
         }
     }
--- Sample/BallApp/SoccerBall.cs
+++ Sample/BallApp/SoccerBall.cs
@@ -14,21 +14,34 @@ namespace BallApp {
         private double posX;        //ｘ座標
         private double posY;        //ｙ座標
 
-        private double moveX = 10;      //移動量（ｘ方向）    10も可
-        private double moveY = 10;      //移動量（ｙ方向）    10も可
+        private double moveX;      //移動量（ｘ方向）
+        private double moveY;      //移動量（ｙ方向）
 
         //コンストラクタ
-        public SoccerBall() {
+        public SoccerBall( double xp , double yp ) {
 
-            Image = Image.FromFile( @"pic\soccer_ball.png" );        //@：すべて完全な文字列とみなす
+            Random random = new Random();       //乱数インスタンス
+
+            this.Image = Image.FromFile( @"pic\soccer_ball.png" );        //@：すべて完全な文字列とみなす
             //image = Image.FromFile("pic\\soccerball.png");        //\:エスケープシーケンス、処理記号
-            PosX = 0.0;     //０でもいいが、double型だから...
-            PosY = 0.0;
+            this.PosX = xp;     //０でもいいが、double型だから...
+            this.PosY = yp;
+
+            //ベーシックなやり方
+            //moveX = random.Next(-15, 15);      //移動量（ｘ方向）
+            //moveY = random.Next(-15, 15);      //移動量（ｙ方向）
+
+            //条件演算子
+            int rndX = random.Next(-15, 15);
+            this.moveX = ( rndX != 0 ? rndX : 1 );       //乱数で移動量を設定
+
+            int rndY = random.Next(-15, 15);
+            this.moveY = ( rndY != 0 ? rndY : 1 );     //乱数で移動量を設定
 
         }
 
         //プロパティ
-        public double PosX { get => posX; set => posX = value; }
+        public double PosX { get => posX; set => posX = value; }        //いわゆるカプセル化
         public double PosY { get => posY; set => posY = value; }
         public Image Image { get => image; set => image = value; }
 
@@ -48,12 +61,12 @@ namespace BallApp {
                 moveX *= -1;
             }*/
 
-            if ( posY > 520 || posY < 0)       //ｙの折り返し処理
+            if ( posY > 510 || posY < 0)       //ｙの折り返し処理
             {
                 moveY = - moveY;
             }
 
-            if (posX > 720 || posX < 0)       //ｘの折り返し処理
+            if (posX > 730 || posX < 0)       //ｘの折り返し処理
             {
                 moveX = -moveX;
             }
