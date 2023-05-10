92d223d1a48b86a756e22df5b623722ce12c0927
安藤弘貴 <OIBC34242@ojs.ac.jp>
2023-05-10 10:13:36 +0900

    メソッドの単機能化

--- Chapter02/距離換算プログラム/DistanceConverter/Program.cs
+++ Chapter02/距離換算プログラム/DistanceConverter/Program.cs
@@ -7,50 +7,42 @@ using System.Threading.Tasks;
 namespace DistanceConverter {
     class Program {
         static void Main(string[] args) {
-
-            if ( args.Length >= 1 && args[ 0 ] == "-tom" )
+            if (args.Length >= 1 && args[0] == "-tom")
             {
-
                 //フィートからメートルへの対応表を出力
-                for ( int feet = 1 ; feet <= 10 ; feet++ )
-                {
-
-                    double metar = FeetToMetar( feet );
-                    Console.WriteLine( "{0} ft = {1:0.0000}m" , feet , metar );
-
-                }
-
+                PrintFeetToMetarList(1, 10);
             }
             else
             {
-
                 //メートルからフィートへの対応表を出力
-                for ( int metar = 1 ; metar <= 10 ; metar++ )
-                {
-
-                    double feet = MetarToFeet( metar );
-                    Console.WriteLine( "{0} ft = {1:0.0000}m" , metar , feet );
-
-                }
-
+                PrintMetarToFeetList(1, 10);
             }
+        }
 
+        private static void PrintMetarToFeetList(int start, int stop) {
+            for (int metar = 1; metar <= 10; metar++)
+            {
+                double feet = MetarToFeet(metar);
+                Console.WriteLine("{0} ft = {1:0.0000}m", metar, feet);
+            }
+        }
+
+        private static void PrintFeetToMetarList(int start, int stop) {
+            for (int feet = 1; feet <= 10; feet++)
+            {
+                double metar = FeetToMetar(feet);
+                Console.WriteLine("{0} ft = {1:0.0000}m", feet, metar);
+            }
         }
 
         //フィートからメートルを求める
-        static double FeetToMetar( int feet ) {
-
+        static double FeetToMetar(int feet) {
             return feet * 0.3048;
-
         }
 
         //メートルからフィートを求める
-        static double MetarToFeet( int metar ) {
-
+        static double MetarToFeet(int metar) {
             return metar / 0.3048;
-
         }
-
     }
-
 }
