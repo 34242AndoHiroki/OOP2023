f9a3e7b9e655faae9ad5d0ee1337d19abe87a6a4
安藤弘貴 <OIBC34242@ojs.ac.jp>
2023-05-17 11:23:14 +0900

    P79問題2.1完成

--- Chapter02/演習問題/Exercise01/Program.cs
+++ Chapter02/演習問題/Exercise01/Program.cs
@@ -7,7 +7,8 @@ using System.Threading.Tasks;
 namespace Exercise01 {
     class Program {
         static void Main(string[] args) {
-
+#if false
+        #region 自力
             #region ３．
             //2.1.3
             var songs = new Song[] {
@@ -31,10 +32,9 @@ namespace Exercise01 {
 
             PrintSongs(lists);
             #endregion
-
         }
 
-        #region ４．
+            #region ４．
         public static void PrintSongs(IEnumerable<Song> songs) {
             foreach (Song song in songs)
             {
@@ -44,7 +44,49 @@ namespace Exercise01 {
                 Console.WriteLine();        //改行
             }
         }
+            #endregion
         #endregion
+#else
+        #region 模範解答
+            var songs = new Song[] {
+                new Song( "Let it be" , "The Beatles" , 243 ) ,
+                new Song( "Bridge Over Troubled Water" , "Simon & Garfunkel" , 293) ,
+                new Song( "Close To You" , "Carpenters" , 276 ) ,
+                new Song( "Honesty" , "Billy Joel" , 231 ) ,
+                new Song( "I Will Always Love You" , "Whitney Houston" , 273 ) ,
+            };
 
+            PrintSongs(songs);
+
+            var lists = new List<Song>();
+            lists.Add(new Song("Let it be", "The Beatles", 243));
+            lists.Add(new Song("Bridge Over Troubled Water", "Simon & Garfunkel", 293));
+            lists.Add(new Song("Close To You", "Carpenters", 276));
+            lists.Add(new Song("Honesty", "Billy Joel", 231));
+            lists.Add(new Song("I Will Always Love You", "Whitney Houston", 273));
+
+            PrintSongs(lists);
+        }
+
+        //2.1.4
+        /*      こんなオーバーロードしないでね
+        private static void PrintSongs(List<Song> lists) {
+            
+        }
+
+        private static void PrintSongs(Song[] songs) {
+            
+        }
+        */
+        private static void PrintSongs(IEnumerable<Song> songs) {
+            foreach (var song in songs)
+            {
+                Console.WriteLine("{0}, {1}, {2:m\\:ss}",       //@"{0}, {1}, {2:m\:ss}" でも可　逐次的文字列リテラル
+                                        song.Title, song.ArtistName, TimeSpan.FromSeconds(song.Length));        //こんなやり方もある
+                //Console.WriteLine();    //改行
+            }
+        }
+        #endregion
+#endif
     }
 }
--- Chapter02/演習問題/Exercise01/Song.cs
+++ Chapter02/演習問題/Exercise01/Song.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 namespace Exercise01 {
     class Song {
 
+#if false
+    #region My
         #region １．
         //歌のタイトル
         public string Title { get; set; }
@@ -23,6 +25,23 @@ namespace Exercise01 {
             this.Length = length;
         }
         #endregion
+    #endregion
+#else
+    #region 模範解答
+
+        //自動実装プロパティ
+        public string Title { get; set; }       //歌のタイトル
+        public string ArtistName { get; set; }      //アーティスト名
+        public int Length { get; set; }      //演奏時間（秒）
+
+        //2.1.2
+        public Song( string title , string artistname , int length ) {      //Mainの呼び出しに合わせる
+            Title = title;          //Javaだと this.Title = this.setTitle( title ) とかする
+            ArtistName = artistname;
+            Length = length;
+        }
+    #endregion
+#endif
 
     }
 }
