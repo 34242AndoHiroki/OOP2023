b8c600944536089e208472dc41f3c8f59d4d4cc8
安藤弘貴 <OIBC34242@ojs.ac.jp>
2023-05-17 11:47:18 +0900

    p79問題2.2完成

--- Chapter02/演習問題/Exercise02/InchConverter.cs
+++ Chapter02/演習問題/Exercise02/InchConverter.cs
@@ -5,9 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Exercise02 {
+#if false
+    #region 自力
     //インチとメートルの単位換算クラス
     public static class InchConverter {
-
         //定数
         private const double ratio = 0.0254;
 
@@ -20,6 +21,25 @@ namespace Exercise02 {
         public static double ToMeter(double metar) {
             return metar * ratio;
         }
-
     }
+    #endregion
+#else
+    #region 模範解答
+    //FeetConverterをまんまコピーして変更
+    public static class InchConverter {     //feetからinch
+        //定数
+        private const double ratio = 0.0254;
+
+        //メートルからインチを求める
+        public static double ToMeter(double metar) {
+            return metar * ratio;
+        }
+
+        //インチからメートルを求める
+        public static double FromMeter(double inch) {       //feetからinch
+            return inch / ratio;
+        }
+    }
+    #endregion
+#endif
 }
--- Chapter02/演習問題/Exercise02/Program.cs
+++ Chapter02/演習問題/Exercise02/Program.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace Exercise02 {
     class Program {
-
+#if false
+        #region 自力
         static void Main(string[] args) {
             PrintInchToMetarList(1, 10);
         }
@@ -30,6 +31,39 @@ namespace Exercise02 {
             }
 
         }
+        #endregion
+#else
+        #region 模範解答
+        //DistanceConverterのProgramをコピーして変更
+        static void Main(string[] args) {
+            if (args.Length >= 1 && args[0] == "-tom")
+            {
+                //フィートからメートルへの対応表を出力
+                PrintInchToMetarList(1, 10);
+            }
+            else
+            {
+                //メートルからフィートへの対応表を出力
+                PrintMetarToInchList(1, 10);
+            }
+        }
 
+        private static void PrintInchToMetarList(int start, int stop) {
+            for (int inch = start; inch <= stop; inch++)
+            {
+                double metar = InchConverter.ToMeter(inch);
+                Console.WriteLine("{0} ft = {1:0.0000}m", inch, metar);
+            }
+        }
+
+        private static void PrintMetarToInchList(int start, int stop) {
+            for (int metar = start; metar <= stop; metar++)
+            {
+                double inch = InchConverter.FromMeter(metar);
+                Console.WriteLine("{0} ft = {1:0.0000}m", metar, inch);
+            }
+        }
+        #endregion
+#endif
     }
 }
