4266526cf674c213db99fd10a506d748d65f1196
安藤弘貴 <OIBC34242@ojs.ac.jp>
2023-05-23 11:54:27 +0900

    第３章の演習問題１

---
+++ Chapter03/Section04/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section04 {
+
+    class Program {
+
+        static void Main(string[] args) {
+
+            var names = new List< string > {
+
+               "Tokyo" , "New Delhi" , "Bangkok" , "London" , "Paris" , "Berlin" , "Canberra" , "Hong Kong" ,
+
+            };
+
+            //var data = names.Find( s => s.Length <= 5 );      //　string型
+            //var data = names.FindAll( s => s.Length <= 5 );       // List< string >型
+            //var query = names.Where( s => s.Length <= 5 );        //IEnumerable< string >型
+
+            //IEnumerable< string > query = names.Where( s => s.Length <= 5 ).Select( s => s.ToLower() );       //小文字をセレクト
+
+            var query = names.Select( s => s.Length );      //文字列長をセレクト
+            //var query = names.Where( s => s.Length );      //条件ではない
+
+            foreach ( var s in names )
+            {
+                Console.WriteLine( s );
+            }
+
+        }
+
+    }
+
+}
