ada5fe96942c8a61271f35605b51633fee4940be
安藤弘貴 <OIBC34242@ojs.ac.jp>
2023-05-24 10:14:53 +0900

    演習問題3.1 完成

--- Chapter03/演習問題/Exercise01/Program.cs
+++ Chapter03/演習問題/Exercise01/Program.cs
@@ -27,47 +27,69 @@ namespace Exercise01 {
 #if true
         #region 自力
         private static void Exercise1_1(List<int> numbers) {
-            Console.WriteLine(numbers.Exists(n=>n%8==0||n%9==0)?"存在しています":"存在していません");
+            //Console.WriteLine(numbers.Exists(n => n % 8 == 0 || n % 9 == 0) ? "存在しています" : "存在していません");
         }
 
         private static void Exercise1_2(List<int> numbers) {
-            
+            numbers.ForEach(n => Console.WriteLine(n / 2.0));
         }
 
         private static void Exercise1_3(List<int> numbers) {
-            
+            numbers.Where(n => n >= 50).ToList().ForEach(n => Console.WriteLine(n));
         }
 
         private static void Exercise1_4(List<int> numbers) {
-            
+            List<int> list = numbers.Select(n=>n*2).ToList();
+            list.ForEach(n => Console.WriteLine(n));
+
+            //numbers.Select(n => n * 2).ToList().ForEach(n => Console.WriteLine(n));
         }
         #endregion
 #else
         #region 模範解答
         private static void Exercise1_1(List<int> numbers) {
 
-            var exist = numbers.Exists(         );
+            var exist = numbers.Exists(n => n % 8 == 0 ||n % 9 == 0);       // -> :アロー演算子　ポインターのやつ
             if (exist)
-            {
                 Console.WriteLine("存在しています");
-            }
             else
-            {
                 Console.WriteLine("存在していません");
-            }
 
         }
 
         private static void Exercise1_2(List<int> numbers) {
+            numbers.ForEach(s => Console.WriteLine(s / 2.0));
 
+            //var datas = numbers.Select(n => n / 2.0);
+            //foreach(var item in datas)
+            //{
+            //    Console.WriteLine(item + " ");
+            //}
         }
 
         private static void Exercise1_3(List<int> numbers) {
+            //var datas = numbers.Where(n => n >= 50);
+            //foreach(var item in datas)
+            //{
+            //    Console.WriteLine(item + " ");
+            //}
 
+            //foreach (var item in numbers.Where(n => n >= 50))       //慣れたらこっちでやろう
+            //{
+            //    Console.WriteLine(item + " ");
+            //}
+
+            numbers.Where(n => n >= 50).ToList().ForEach(Console.WriteLine);     //1行で
         }
 
         private static void Exercise1_4(List<int> numbers) {
+            //var list = numbers.Select(n => n * 2).ToList();        //List<int>のキャストはできない
+            //foreach (var item in list)
+            //{
+            //    Console.WriteLine(item + " ");
+            //}
 
+            numbers.Select(n => n * 2).ToList().ForEach(Console.WriteLine);     //1行で
         }
         #endregion
 #endif
