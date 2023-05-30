b35e1e50903fa1def54b7972bd8186a0d5a13ee4
安藤弘貴 <OIBC34242@ojs.ac.jp>
2023-05-30 11:24:06 +0900

    4.1.2完成

--- Chapter04/演習問題/Exercise01/YearMonth.cs
+++ Chapter04/演習問題/Exercise01/YearMonth.cs
@@ -12,19 +12,30 @@ namespace Exercise01 {
         public int Year { get; private set; }
         //月
         public int Month { get; private set; }
+        //21世紀の判定
+        public bool Is21Century => 2001 <= Year && Year <= 2100;
 
         //コンストラクタ
-        public YearMonth( int year , int month ) {
+        public YearMonth(int year, int month) {
             Year = year;
             Month = month;
         }
 
-        //メソッド
         
+
         #endregion
 #else
         #region 模範解答
+        public int Year { get; private set; }
         
+        public int Month { get; private set; }
+        
+        public int Is21Centry { get; set; }
+
+        public YearMonth(int year , int month) {
+            Year = year;
+            Month = month;
+        }
         #endregion
 #endif
     }
