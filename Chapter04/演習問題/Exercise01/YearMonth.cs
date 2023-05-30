5b754741fb7e5cb059a4bb387f926f84fdcdb450
安藤弘貴 <OIBC34242@ojs.ac.jp>
2023-05-30 10:24:21 +0900

    4.1.1完成

---
+++ Chapter04/演習問題/Exercise01/YearMonth.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    public class YearMonth {
+#if true
+        #region 自力
+        //年
+        public int Year { get; private set; }
+        //月
+        public int Month { get; private set; }
+
+        //コンストラクタ
+        public YearMonth( int year , int month ) {
+            Year = year;
+            Month = month;
+        }
+
+        //メソッド
+        
+        #endregion
+#else
+        #region 模範解答
+        
+        #endregion
+#endif
+    }
+}
