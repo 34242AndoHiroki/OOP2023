5d7c7f370cdbc2852b075f070cdfa8ea8a7d7533
安藤弘貴 <OIBC34242@ojs.ac.jp>
2023-05-30 11:26:31 +0900

    模範解答4.1.2完成

--- Chapter04/演習問題/Exercise01/YearMonth.cs
+++ Chapter04/演習問題/Exercise01/YearMonth.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Exercise01 {
     public class YearMonth {
-#if true
+#if !true
         #region 自力
         //年
         public int Year { get; private set; }
@@ -21,7 +21,18 @@ namespace Exercise01 {
             Month = month;
         }
 
-        
+        public YearMonth() { }
+
+        //メソッド
+        //4.1.3
+        public YearMonth AddOneMonth() {
+            return
+                new YearMonth {
+                    Month = (this.Month) % 12  == 0 ? ,
+                    Year  = ,
+                };
+            
+        }
 
         #endregion
 #else
@@ -29,12 +40,19 @@ namespace Exercise01 {
         public int Year { get; private set; }
         
         public int Month { get; private set; }
-        
-        public int Is21Centry { get; set; }
 
         public YearMonth(int year , int month) {
             Year = year;
             Month = month;
+        }
+
+        //4.1.2
+        public bool Is21Centry
+        {
+            get
+            {
+                return 2001 <= Year && Year <= 2100;
+            }
         }
         #endregion
 #endif
