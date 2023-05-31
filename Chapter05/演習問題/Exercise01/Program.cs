63eeba44e19c8d88945615fa70a34f5892f8d914
安藤弘貴 <OIBC34242@ojs.ac.jp>
2023-05-31 11:55:35 +0900

    演習5.1 完成

---
+++ Chapter05/演習問題/Exercise01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;     //消してもよし
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("文字列１：");
+            var s1 = Console.ReadLine();
+
+            Console.WriteLine("文字列２：");
+            var s2 = Console.ReadLine();
+#if true
+            #region 自力
+            if (String.Compare(s1,s2,ignoreCase:true) == 0)
+            {
+                Console.WriteLine("等しい");
+            }
+            else
+            {
+                Console.WriteLine("等しくない");
+            }
+            #endregion
+#else
+            #region 模範解答
+            if (string.Compare(s1, s2, ignoreCase: true) == 0)
+                Console.WriteLine("等しい");
+            else
+                Console.WriteLine("等しくない");
+            #endregion
+#endif
+
+        }
+    }
+}
