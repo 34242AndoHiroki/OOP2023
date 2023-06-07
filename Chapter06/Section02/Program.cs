1b6db7cc3dd18aaa8280fe7b1f5ed08929ba5269
安藤弘貴 <OIBC34242@ojs.ac.jp>
2023-06-07 10:55:47 +0900

    LINQのメモリ使用量と処理速度

---
+++ Chapter06/Section02/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section02 {
+
+    class Program {
+
+        static void Main(string[] args) {
+
+            Stopwatch sw = new Stopwatch();     //タイマーインスタンス生成
+            sw.Start();     //タイマースタート
+
+            var numbers = Enumerable.Range( 1 , 1000000 ).ToArray();        //要素数 1000000 の配列取得　3.836 MB  
+            //var numbers = Enumerable.Range( 1 , 1000000 );                // 0.021 MB
+            //var numbers = Enumerable.Range( 1 , 1000000 ).ToList();         // 4.021 MB
+            WriteTotalMemory("配列確保後");
+
+            //偶数だけを取り出す
+
+#if !true
+
+            #region 自力
+
+            numbers.Where( x => x % 2 == 0 ).ToList().ForEach( Console.WriteLine );
+
+            #endregion
+
+#else
+
+            #region 模範解答
+
+            var evenNumbers = numbers.Where( n => n % 2 == 0 );     //ToList() があると 実行時間 = 00.01753 ms
+                                                                    //ToArray() だと 実行時間 = 00.01652 ms
+            Console.WriteLine( "偶数抽出後" );
+
+            //foreach ( var item in numbers )
+            //{
+            //    Console.WriteLine( item + " " );
+            //}
+
+            //取り出した偶数の平均値を求める
+            var ave = evenNumbers.Average();
+
+            WriteTotalMemory( "偶数抽出後平均" );
+            Console.WriteLine( "実行時間 = {0}" , sw.Elapsed.ToString( @"ss\.fffff" ) );        //時間表示
+
+            #endregion
+
+#endif
+
+        }
+
+        static void WriteTotalMemory( string header ) {
+
+            var totalMemory = GC.GetTotalMemory( true ) / 1024.0 / 1024.0;          // MB に変換
+            Console.WriteLine( $"{header}: {totalMemory:0.000 MB}" );
+
+        }
+
+    }
+
+}
---
+++ Chapter06/演習問題/Section01/Program.cs
@@ -0,0 +1,177 @@
+﻿//#define No1
+//#define No2
+//#define No3
+//#define No4
+#define No5
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+
+    class Program {
+
+        static void Main(string[] args) {
+
+            var numbers = new List< int > { 9 , 7 , 5 , 4 , 2 , 5 , 4 , 0 , 4 , 1 , 0 , 4 };
+
+            Console.WriteLine( numbers.Average() );       //平均を求める
+
+
+            var books = Books.GetBooks();       //本を取得
+
+            Console.WriteLine( books.Average( x => x.Price ) );     //価格の平均
+            Console.WriteLine( books.Average( x => x.Pages ) );     //ページの平均
+
+            Console.WriteLine( books.Max( x => x.Pages ) );     //最大ページ数
+            Console.WriteLine( books.Max( x => x.Price ) );     //最大価格
+
+            Console.WriteLine( "-------------" );
+
+#if No1
+
+            //500円以上の本のタイトルをすべて出力
+
+#if !true
+
+            #region 自力
+
+            books.Where( x => x.Price >= 500 ).Select( x => x.Title ).ToList().ForEach( Console.WriteLine );
+            //books.Where( x => x.Price >= 500 ).Select( x => x ).ToList().ForEach( Console.WriteLine );
+
+            books.Where( x => x.Price >= 500 ).Select( x => x ).OrderBy( x => x.Price ).ToList().ForEach( x => Console.WriteLine( "{0}：{1}円" , x.Title, x.Price ) );
+            
+            #endregion
+
+#else
+
+            #region 模範解答
+
+            var booksObj = books.Where( x => x.Price >= 500 );
+
+            foreach( var book in booksObj )
+            {
+                Console.WriteLine( "{0}：{1}円" , book.Title , book.Price );
+            }
+
+            #endregion
+
+#endif
+
+#elif No2
+
+        //500円以上の本の価格を降順に出力
+
+#if !true
+
+            #region 自力
+
+                //books.Where( x => x.Price >= 500 ).Select( x => x.Title ).ToList().ForEach( Console.WriteLine );
+
+                books.Where( x => x.Price >= 500 ).Select( x => x ).OrderBy( x => x.Price ).ToList().ForEach( x => Console.WriteLine( "{0}：{1}円" , x.Title, x.Price ) );
+            
+            #endregion
+
+#else
+
+            #region 模範解答
+
+                var booksObj = books.Where( x => x.Price >= 500 ).OrderByDescending( x => x.Price );
+
+                foreach( var book in booksObj )
+                {
+                    Console.WriteLine( "{0}：{1}円" , book.Title , book.Price );
+                }
+
+            #endregion
+
+#endif
+
+#elif No3
+
+#if !true
+
+            #region 自力
+
+                books.Where( x => x.Price >= 500 ).Select( x => x.Title ).ToList().ForEach( Console.WriteLine );
+                //books.Where( x => x.Price >= 500 ).Select( x => x ).ToList().ForEach( Console.WriteLine );
+
+                books.Where( x => x.Price >= 500 ).Select( x => x ).OrderBy( x => x.Price ).ToList().ForEach( x => Console.WriteLine( "{0}：{1}円" , x.Title, x.Price ) );
+            
+            #endregion
+
+#else
+
+            #region 模範解答
+
+                var booksObj = books.Where(x => x.Title.Contains( "物語" ) );
+
+                foreach (var book in booksObj)
+                {
+                    Console.WriteLine( "{0}：{1}円" , book.Title , book.Price );
+                }
+
+            #endregion
+
+#endif
+
+#elif No4
+
+            //タイトルに「物語」が含まれている書籍の平均ページ数
+
+#if !true
+
+            #region 自力
+
+            Console.WriteLine( "平均：{0}円" , books.Where( x => x.Title.Contains( "物語" ) ).Average( x => x.Pages ) );
+
+            #endregion
+
+#else
+
+            #region 模範解答
+
+            var booksObj = books.Where( x => x.Title.Contains( "物語" ) ).Average( x => x.Pages );
+            Console.WriteLine( "平均：{0}円" , booksObj );
+
+            #endregion
+
+#endif
+
+#elif No5
+
+            //タイトルが長い順
+
+#if !true
+
+            #region 自力
+
+            books.OrderByDescending( x => x.Title.Length ).ToList().ForEach( x => Console.WriteLine( x.Title ) );
+
+            #endregion
+
+#else
+
+            #region 模範解答
+
+            var longTitlebooks = books.OrderByDescending( x => x.Title.Length );        // x => x.Title だと名前がASCIIコード順に整列される
+
+            foreach ( var book in books )
+            {
+                Console.WriteLine( "{0} {1}" , book.Title , book.Price );
+            }
+
+            #endregion
+
+#endif
+
+#endif
+
+        }
+
+    }
+
+}
