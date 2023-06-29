2e518689647bf012d1c0eb914850bd779b0e3726
安藤弘貴 <OIBC34242@ojs.ac.jp>
2023-06-29 11:14:35 +0900

    問題7.1 一応完成

---
+++ Chapter07/演習問題/Exercise01/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    class Program {
+        static void Main(string[] args) {
+            var text = "Cozy lummox gives smart squid who asks for job pen";
+            Exercise1_1(text);
+            Console.WriteLine();
+            Exercise1_2(text);
+        }
+
+#if true
+        #region 自力
+        private static void Exercise1_1(string text) {
+            var dict = new Dictionary<Char, int>();
+
+            foreach (var ch in text)
+            {
+                for (char c = 'A'; c <= 'Z'; c++)
+                {
+                    if (ch.ToString().ToUpper()==c.ToString())
+                    {
+                        if (dict.ContainsKey(c)) dict[c]++;
+                        else dict.Add(c, 1);
+                        break;
+                    }
+                }
+            }
+
+            dict.OrderBy(c => c.Key).ToList().ForEach(c => Console.WriteLine("'{0}'：{1}",c.Key,c.Value));
+        }
+
+        private static void Exercise1_2(string text) {
+            var dict = new SortedDictionary<Char, int>();
+
+            foreach (var ch in text)
+            {
+                for (char c = 'A'; c <= 'Z'; c++)
+                {
+                    if (ch.ToString().ToUpper() == c.ToString())
+                    {
+                        if (dict.ContainsKey(c)) dict[c]++;
+                        else dict.Add(c, 1);
+                        break;
+                    }
+                }
+            }
+
+            dict.ToList().ForEach(c => Console.WriteLine("'{0}'：{1}", c.Key, c.Value));
+        }
+        #endregion
+#else
+        #region 模範解答
+        private static void Exercise1_1(string text) {
+            var dict = new Dictionary<Char, int>();
+        }
+
+        private static void Exercise1_2(string text) {
+
+        }
+        #endregion
+#endif
+
+
+    }
+}
