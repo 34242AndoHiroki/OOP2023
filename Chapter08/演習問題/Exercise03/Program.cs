2e44c4fac733a9e32a243eec67d8b011baf4ba1e
安藤弘貴 <OIBC34242@ojs.ac.jp>
2023-07-03 14:36:46 +0900

    問題8.3 一応完成

--- Chapter08/演習問題/Exercise03/Program.cs
+++ Chapter08/演習問題/Exercise03/Program.cs
@@ -2,17 +2,72 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Exercise03 {
+#if true
+    #region 自力
     class Program {
         static void Main(string[] args) {
+
+            var tw = new TimeWatch();
+            tw.Start();
+            Thread.Sleep(1000);
+            TimeSpan duration = tw.Stop();
+            Console.WriteLine("処理時間は{0}ミリ秒でした", duration.TotalMilliseconds);
+
         }
     }
 
     class TimeWatch {
 
+        DateTime start;
+        DateTime stop;
+
+        public void Start() {
+            start = DateTime.Now;
+        }
+
+        public TimeSpan Stop() {
+
+            stop = DateTime.Now;
+            return stop - start;
+
+        }
+
+    }
+    #endregion
+#else
+    #region 模範解答
+    class Program {
+        static void Main(string[] args) {
+            var tw = new TimeWatch();
+            tw.Start();         //スタートするからStart()は必要
+            Thread.Sleep(1000);
+            TimeSpan duration = tw.Stop();      //ストップするからStop()は必要　時間差も欲しい
+            Console.WriteLine("処理時間は{0}ミリ秒でした",duration.TotalMilliseconds);
+        }
+    }
+
+    class TimeWatch {
+
+        public void Start() {
+
+
+
+        }
+
+        public TimeSpan Stop() {
+
+
+
+        }
 
 
     }
+
+    #endregion
+#endif
+
 }
