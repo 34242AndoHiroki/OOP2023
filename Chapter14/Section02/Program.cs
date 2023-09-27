cf32041eb9ce13117261fc91890075042bd06fe8
安藤弘貴 <OIBC34242@ojs.ac.jp>
2023-09-27 10:35:19 +0900

    RSSファイルの取得

---
+++ Chapter14/Section02/Program.cs
@@ -0,0 +1,58 @@
+﻿//#define HTTP通信
+#define 非同期処理
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section02 {
+
+    class Program {
+
+#if HTTP通信
+
+        static void Main( string[] args ) {
+
+            //これだけで通信ができる（非推奨の方法）
+            var wc = new WebClient();       //ここ変えるだけでできる
+            wc.Encoding = Encoding.UTF8;       //ASCII だと日本語が文字化けする
+            var html = wc.DownloadString( "https://visualstudio.microsoft.com/ja/" );       //URL
+            Console.WriteLine( html );      //ページのハイパーテキストを表示
+
+        }
+
+#elif 非同期処理
+
+        static void Main( string[] args ) {
+
+            var wc = new WebClient();
+            var url = new Uri( "https://www.rimarts.jp/downloads/B2/bk28104j.zip" );
+            var filename = @"C:\temp\example.zip";
+            wc.DownloadProgressChanged += wc_DownloadProgressChanged;       //デリゲート登録
+            wc.DownloadFileCompleted += wc_DownloadFileCompleted;
+            wc.DownloadFileAsync( url , filename );
+            Console.ReadLine();
+
+        }
+
+        static void wc_DownloadProgressChanged( object sender , DownloadProgressChangedEventArgs e ) {
+
+            Console.WriteLine( "{0}% {0}/{1}" , e.ProgressPercentage ,
+                              e.BytesReceived , e.TotalBytesToReceive );
+
+        }
+
+        static void wc_DownloadFileCompleted( object sender , System.ComponentModel.AsyncCompletedEventArgs e ) {       //完了時に呼ばれるメソッド
+
+            Console.WriteLine( "ダウンロード完了" );
+
+        }
+
+#endif
+
+    }
+
+}
---
+++ Chapter14/Section04/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Section04 {
+
+    class Program {
+
+        static void Main( string[] args ) {
+
+            //OpenReadSample();
+
+            var results = GetWeatherReportFromYahoo( 4610 );        //4610:横浜      4210:前橋     4620:小田原
+
+            foreach ( var s in results ) {
+                Console.WriteLine( s );
+            }
+
+            Console.WriteLine();
+
+        }
+
+        //List 14-18
+        private static void OpenReadSample() {
+
+            var wc = new WebClient();       //非推奨らしい HttpClient をお勧めしてるらしい
+
+            using ( var stream = wc.OpenRead( @"http://gihyo.jp/book/list" ) )     //逐語的リテラル
+
+            using ( var sr = new StreamReader( stream , Encoding.UTF8 ) ) {
+
+                string html = sr.ReadToEnd();
+                Console.WriteLine( html );
+
+            }
+
+        }
+
+        //List 14-19
+        private static IEnumerable< string > GetWeatherReportFromYahoo( int cityCode ) {        //yieldだと戻り値は int か IEnumerable<>
+
+            using ( var wc = new WebClient() ) {
+
+                wc.Headers.Add( "Content-type" , "charset=UTF-8" );
+                var uriString = string.Format( @"https://news.yahoo.co.jp/rss/topics/it.xml", cityCode );     // URLを変えるといろいろ変わる
+                var url = new Uri( uriString );
+                var stream = wc.OpenRead( url );
+
+                XDocument xdoc = XDocument.Load( stream );
+                var nodes = xdoc.Root.Descendants( "title" );       //タイトルとってくる
+
+                foreach ( var node in nodes ) {         //ここで解析
+
+                    string s = Regex.Replace( node.Value , "【|】" , "" );
+                    yield return node.Value;        //yield: １行ずつリターンする
+
+                }
+
+            }
+
+        }
+
+    }
+
+}
