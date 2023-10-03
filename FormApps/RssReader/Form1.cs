0f4bf45abede8cf175428a556755d48f9fc3be12
安藤弘貴 <OIBC34242@ojs.ac.jp>
2023-10-03 10:49:49 +0900

    自力完成

--- FormApps/RssReader/Form1.cs
+++ FormApps/RssReader/Form1.cs
@@ -1,17 +1,137 @@
-﻿using System;
+﻿#define Mywork
+#define Answer
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace RssReader {
+
     public partial class Form1 : Form {
+
+#if Mywork
+
+        List< ItemData > nodes;
+
         public Form1() {
             InitializeComponent();
         }
+
+        #region 自力
+
+        private void btGet_Click( object sender , EventArgs e ) {
+
+            using ( var wc = new WebClient() ) {
+
+                var url = wc.OpenRead( /*tbUrl.Text*/ "https://news.yahoo.co.jp/rss/media/kurumans/all.xml" );
+                XDocument xdoc = XDocument.Load( url );
+
+                nodes = xdoc.Root.Descendants( "item" )
+                                            .Select( x => new ItemData
+                                                {
+
+                                                    Title = x.Element( "title" ).Value ,
+                                                    Link = x.Element( "link" ).Value    
+                                                    
+                                                } ).ToList();
+
+                //var nodes = xdoc.Root.Descendants( "item" ).Descendants( "title" );
+
+                foreach ( var node in nodes ) {
+
+                    lbRssTitle.Items.Add( node.Title );
+
+                }
+
+            }
+
+        }
+
+        private void lbRssTitle_DragDrop( object sender , DragEventArgs e ) {
+
+            //var dropFiles = e.Data.GetData();
+
+        }
+
+        private void lbRssTitle_DragEnter( object sender , DragEventArgs e ) {
+
+
+
+        }
+
+        private void lbRssTitle_MouseDown(object sender, MouseEventArgs e) {
+
+            //Point p = Control.MousePosition;
+            //p = lbRssTitle.PointToClient(p);//マウスの位置をクライアント座標に変換
+            //int ind = lbRssTitle.IndexFromPoint(p);//マウス下のＬＢのインデックスを得る
+            //if (ind > -1) {
+            //    lbRssTitle.DoDragDrop(lbRssTitle.Items[ind].ToString(), DragDropEffects.Copy);//ドラッグスタート
+            //}
+
+        }
+
+        private void lbRssTitle_Click( object sender , EventArgs e ) {
+
+            wbBrowser.Navigate( ( nodes[ lbRssTitle.SelectedIndex ] ).Link );
+
+
+        }
+
+        #endregion
+
+#elif Answer
+
+        #region 模範解答
+
+        List< ItemData > ItemDatas = new List< ItemData >();        //IEnumerable<>はインターフェースだから使えない
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void btGet_Click( object sender , EventArgs e ) {
+
+            using ( var wc = new WebClient() ) {
+
+                var url = wc.OpenRead( tbUrl.Text );
+                XDocument xdoc = XDocument.Load( url );
+
+                var ItemDatas = xdoc.Root.Descendants( "item" )       //要素を持ってくる条件
+                                            .Select( x => new ItemData
+                                                {
+                                                    Title = ( string )x.Element( "title" ) ,           //明示的なキャストだから ToString() よりもキャスト
+                                                    Link = ( string )x.Element( "link" ) ,
+                                                } ).ToList();
+                //var nodes = xdoc.Root.Descendants( "item" ).Descendants( "title" );
+
+                foreach ( var node in ItemDatas) {         //ここで解析
+
+                    lbRssTitle.Items.Add( node.Title );
+
+                }
+
+            }
+
+        }
+
+        private void lbRssTitle_Click( object sender , EventArgs e ) {
+
+
+        }
+
+        #endregion
+
+#endif
+
     }
+
 }
---
+++ FormApps/RssReader/ItemData.cs
@@ -0,0 +1,36 @@
+﻿//#define Mywork
+#define Answer
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RssReader {
+
+    public class ItemData {
+
+#if Mywork
+
+        #region 自力
+
+        public string Title { get; set; }
+        public string Link { get; set; }
+
+        #endregion
+
+#elif Answer
+
+        #region 模範解答
+
+        public string Title { get; set; }       //
+        public string Link { get; set; }
+
+        #endregion
+
+#endif
+
+    }
+
+}
