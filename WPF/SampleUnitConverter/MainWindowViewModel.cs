37e56e4f95e88b989d0628195a313cb89eafdda4
安藤弘貴 <OIBC34242@ojs.ac.jp>
2023-11-08 11:25:36 +0900

    Sample Converter 完成

---
+++ WPF/SampleUnitConverter/MainWindowViewModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SampleUnitConverter {
+
+    public class MainWindowViewModel : ViewModel {
+
+        private double metricValue, imperialValue;
+
+        //プロパティ
+        public double MetricValue {
+
+            get { return this.metricValue; }
+
+            set {
+
+                this.metricValue = value;
+                this.OnPropertyChanged();       //通知が届くようにする
+
+            }
+
+        }
+
+        public double ImperialValue {
+
+            get { return this.imperialValue; }
+
+            set {       //代入＝セッター
+
+                this.imperialValue = value;
+                this.OnPropertyChanged();       //通知が届くようにする
+
+            }
+
+        }
+
+        //上のComboBoxで選択されている値（単位）
+        public MetricUnit CurrentMetricUnit { get; set; }       //コンボボックスの値にアクセスできるプロパティ
+
+        //上のComboBoxで選択されている値（単位）
+        public ImperialUnit CurrentImperialUnit { get; set; }   //コンボボックスの値にアクセスできるプロパティ
+
+        //▲ボタンで呼ばれるコマンド
+        public ICommand ImperialToMetricUnit { get; private set; }
+
+        //▼ボタンで呼ばれるコマンド
+        public ICommand MetricToImperialUnit { get; private set; }
+
+        //コンストラクタ
+        public MainWindowViewModel() {
+
+            this.CurrentMetricUnit = MetricUnit.Units.First();
+            this.CurrentImperialUnit = ImperialUnit.Units.First();
+
+            this.MetricToImperialUnit = new DelegateCommand(            //バインディングしたとき（矢印ボタンを押下したとき）に処理される
+                () => this.imperialValue = this.CurrentImperialUnit.FromMetricUnit( this.CurrentMetricUnit , this.MetricValue ) );      //処理の登録
+
+            this.ImperialToMetricUnit = new DelegateCommand(            //バインディングしたとき（矢印ボタンを押下したとき）に処理される
+                () => this.MetricValue = this.CurrentMetricUnit.FromImperialUnit( this.CurrentImperialUnit , this.ImperialValue ) );    //処理の登録
+
+        }
+
+    }
+
+}
---
+++ WPF/SampleUnitConverter/ViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace SampleUnitConverter {
+
+    public class ViewModel : INotifyPropertyChanged {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged( [ CallerMemberName ]string propertyName = null ) {       //プロパティの値が変更されると関係してくる
+
+            PropertyChanged?.Invoke( this , new PropertyChangedEventArgs( propertyName ) );
+
+            //if( this.PropertyChanged != null ) {
+            //    this.PropertyChanged( this , new PropertyChangedEventArgs( propertyName ) );
+            //}
+
+        }
+
+    }
+
+}
